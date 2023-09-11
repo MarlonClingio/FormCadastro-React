626d0315bcb93a327fae85fa8a5f15ba7c5ab547
unknown <Marlon Clingio@DESKTOP-NBJ0U4R.(none)>
2023-09-10 22:48:02 -0300

    Formulário Cadastro

---
+++ src/components/AdicionaCliente.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Formik, useField } from 'formik'
+import * as yup from 'yup'
+
+const Campo = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className='form-group'>
+      <label htmlFor={props.id}>{label}</label>
+      <input
+        {...field}
+        {...props}
+        className={meta.error && meta.touched ? 'is-invalid' : ''}
+      />
+      {meta.error && meta.touched ? (
+        <div className='invalid-feedback'>{meta.error}</div>
+      ) : null}
+    </div>
+  )
+}
+
+const AdicionaCliente = () => {
+
+  const esquema = yup.object({
+    nome: yup.string()
+      .required('O nome é obrigatório!')
+      .min(10, 'O nome deve ter no minimo 10 caracteres.')
+      .max(40, 'O nome deve ter no maximo 40 caracteres.'),
+    email: yup.string()
+      .required('O nome é obrigatório!')
+      .email('O email é invalido'),
+    nascimento: yup.date()
+    .required('A data de nascimento é obrigatória!')
+    .max(new Date(), 'OPA! Você não pode ter nascido no futuro...')
+  })
+
+  return (
+    <>
+      <h1>Cadastro de Clientes</h1>
+      <Formik
+        initialValues={{
+          nome: '',
+          email: '',
+          nascimento: ''
+        }}
+        validationSchema={esquema}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values))
+        }}
+      >
+        {(props) => (
+          <form noValidate onSubmit={props.handleSubmit}>
+            <Campo id="nome" name="nome" type="text" label="Nome" />
+            <Campo id="email" name="email" type="email" label="Email" />
+            <Campo id="nascimento" name="nascimento" type="date" label="Data de Nascimento" />
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default AdicionaCliente;
